0849e2ab226af4506c4e9338abb4dd9d97d68b02
Lavesh Gaurav <lavesh.g96@gmail.com>
2020-12-27 19:36:02 +0530

    unused variables removed

--- src/App.js
+++ src/App.js
@@ -2,32 +2,15 @@ import React, { useState, useEffect } from "react";
 import ItemTable from "./components/ItemTable";
 import "./App.css";
 import ItemCart from "./components/ItemCart";
-import { makeStyles } from "@material-ui/core/styles";
 import {
   Button,
   Dialog,
   DialogActions,
   DialogContent,
-  DialogContentText,
   DialogTitle,
-  Grid,
-  Paper,
 } from "@material-ui/core";
 
-const useStyles = makeStyles((theme) => ({
-  root: {
-    flexGrow: 1,
-    display: "flex",
-    // flexDirection: "row",
-  },
-  paper: {
-    padding: theme.spacing(2),
-    textAlign: "center",
-    color: theme.palette.text.secondary,
-  },
-}));
 function App() {
-  const classes = useStyles();
   const [open, setOpen] = React.useState(false);
   const [totalPrice, SetTotalPrice] = useState(2760);
   const [discount, setDiscount] = useState(100);
